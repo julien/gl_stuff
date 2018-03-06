89ca3fecde786a2f532ea46b737c2c73dfdfc4d9
Julien Castelain <julien.castelain@liferay.com>
2018-03-06 22:49:10 +0100

    Fix layout in vertex shader

--- sprites04/vert.glsl
+++ sprites04/vert.glsl
@@ -3,9 +3,9 @@
 layout (location=0) in float a_rotation;
 layout (location=1) in vec2 a_translation;
 layout (location=2) in vec2 a_scale;
-layout (location=2) in vec2 a_position;
-layout (location=3) in vec2 a_uv;
-layout (location=4) in vec4 a_color;
+layout (location=3) in vec2 a_position;
+layout (location=4) in vec2 a_uv;
+layout (location=5) in vec4 a_color;
 
 out vec2 v_uv;
 out vec4 v_color;
@@ -24,6 +24,7 @@ void main() {
 
   vec4 pos = vec4(xy, 1.0, 1.0);
   gl_Position = u_matrix * pos;
+
   v_uv = a_uv;
   v_color = a_color;
 }
