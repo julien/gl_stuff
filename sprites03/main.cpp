f4a2a56baaeffa4dab7b716de0070b78b6b4d69c
Julien Castelain <julien.castelain@liferay.com>
2018-03-06 21:52:09 +0100

    Change rendering logic

--- sprites03/main.cpp
+++ sprites03/main.cpp
@@ -8,24 +8,24 @@ struct sprite {
 	float ax;
 	float ay;
 	float life;
+	float rotation;
 	float size;
 	float vx;
 	float vy;
 	float x;
 	float y;
-	// TODO: add color?
 };
 
 int g_viewport_width = 1024;
 int g_viewport_height = 768;
-
-const unsigned int MAX_SPRITES = 300;
+const unsigned int MAX_SPRITES = 4000;
 const int SPRITE_MESH_SIZE = 12;
-const int SPRITE_SIZE = 10;
+const int SPRITE_SIZE = 3;
+const int SPRITE_VERTEX_SIZE = 12;
 unsigned int free_sprite = 0;
-
+unsigned int sprite_count = 0;
 sprite sprites[MAX_SPRITES];
-GLfloat sprite_vertices[MAX_SPRITES * SPRITE_MESH_SIZE];
+GLfloat vpos_data[MAX_SPRITES * SPRITE_MESH_SIZE];
 
 GLfloat view_matrix[16] = {
 	2.0f / (float) g_viewport_width, 0.0f, 0.0f, 0.0f,
@@ -33,6 +33,59 @@ GLfloat view_matrix[16] = {
     0.0f, 0.0f, 1.0f, 1.0f,
    -1.0f, 1.0f, 0.0f, 0.0f
 };
+
+const int MAX_STACK = 100;
+const int MAT_SIZE = 6;
+const int MAT_STACK_SIZE = MAX_STACK * MAT_SIZE;
+GLfloat mat[] = { 1.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f };
+GLfloat stack[MAX_STACK];
+int stackp = 0;
+
+void push_matrix() {
+	stack[stackp + 0] = mat[0];
+	stack[stackp + 1] = mat[1];
+	stack[stackp + 2] = mat[2];
+	stack[stackp + 3] = mat[3];
+	stack[stackp + 4] = mat[4];
+	stack[stackp + 5] = mat[5];
+	stackp += 6;
+}
+
+void pop_matrix() {
+	stackp -= 6;
+	mat[0] = stack[stackp + 0];
+	mat[1] = stack[stackp + 1];
+	mat[2] = stack[stackp + 2];
+	mat[3] = stack[stackp + 3];
+	mat[4] = stack[stackp + 4];
+	mat[5] = stack[stackp + 5];
+}
+
+void trans( float x, float y ) {
+	mat[4] = mat[0] * x + mat[2] * y + mat[4];
+	mat[5] = mat[1] * x + mat[3] * y + mat[5];
+}
+
+void scale( float x, float y ) {
+	mat[0] = mat[0] * x;
+	mat[1] = mat[1] * x;
+	mat[2] = mat[2] * y;
+	mat[3] = mat[3] * y;
+}
+
+void rot( float r ) {
+	float a = mat[0];
+	float b = mat[1];
+	float c = mat[2];
+	float d = mat[3];
+	float sr = sinf(r);
+	float cr = cosf(r);
+
+	mat[0] = a * cr + c * sr;
+	mat[1] = b * cr + d * sr;
+	mat[2] = a * -sr + c * cr;
+	mat[3] = b * -sr + d * cr;
+}
 
 void setup_sprites() {
 	for ( size_t i = 0; i < MAX_SPRITES; i++ ) {
@@ -49,6 +102,7 @@ void setup_sprites() {
 		s->vy = 0;
 		s->ax = 0;
 		s->ay = 0;
+		s->rotation = rand_range( 1, 9 ) * 0.1;
 	}
 }
 
@@ -68,64 +122,55 @@ int find_free_sprite() {
 	return -1;
 }
 
-// TODO: use matrix for translation, rotation, scale
-int draw_sprites() {
-	int quads = 0;
-	int quadsi = 0;
+void drawRect(float x, float y, float w, float h) {
+	float a = mat[0];
+	float b = mat[1];
+	float c = mat[2];
+	float d = mat[3];
+	float e = mat[4];
+	float f = mat[5];
+	float x0 = x;
+	float y0 = y;
+	float x1 = x + w;
+	float y1 = y + h;
+	float x2 = x;
+	float y2 = y + h;
+	float x3 = x;
+	float y3 = y;
+	float x4 = x + w;
+	float y4 = y;
+	float x5 = x + w;
+	float y5 = y + h;
+	int offset = 0;
 
-	for ( unsigned int i = 0; i < MAX_SPRITES; i++ ) {
-		sprite *s = &(sprites[i]);
+	offset = sprite_count * SPRITE_VERTEX_SIZE;
 
-		if (s->life > 0) {
+	vpos_data[offset++] = x0 * a + y0 * c + e;
+	vpos_data[offset++] = x0 * b + y0 * d + f;
 
-			int quadsii = quadsi;
-			float x = s->x;
-			float y = s->y;
-			float xx = x + s->size;
-			float yy = y + s->size;
 
-			sprite_vertices[quadsii++] = x;
-			sprite_vertices[quadsii++] = y;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 1;
+	vpos_data[offset++] = x1 * a + y1 * c + e;
+	vpos_data[offset++] = x1 * b + y1 * d + f;
 
-			sprite_vertices[quadsii++] = xx;
-			sprite_vertices[quadsii++] = y;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 1;
-			// sprite_vertices[quadsii++] = 1;
+	vpos_data[offset++] = x2 * a + y2 * c + e;
+	vpos_data[offset++] = x2 * b + y2 * d + f;
 
-			sprite_vertices[quadsii++] = x;
-			sprite_vertices[quadsii++] = yy;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 0;
+	vpos_data[offset++] = x3 * a + y3 * c + e;
+	vpos_data[offset++] = x3 * b + y3 * d + f;
 
-			sprite_vertices[quadsii++] = x;
-			sprite_vertices[quadsii++] = yy;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 0;
+	vpos_data[offset++] = x4 * a + y4 * c + e;
+	vpos_data[offset++] = x4 * b + y4 * d + f;
 
-			sprite_vertices[quadsii++] = xx;
-			sprite_vertices[quadsii++] = y;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 1;
-			// sprite_vertices[quadsii++] = 1;
+	vpos_data[offset++] = x5 * a + y5 * c + e;
+	vpos_data[offset++] = x5 * b + y5 * d + f;
 
-			sprite_vertices[quadsii++] = xx;
-			sprite_vertices[quadsii++] = yy;
-			// sprite_vertices[quadsii++] = 0;
-			// sprite_vertices[quadsii++] = 1;
-			// sprite_vertices[quadsii++] = 0;
-
-			// quadsi += 30;
-			quadsi += SPRITE_MESH_SIZE;
-			quads++;
-		}
+	if ( ++sprite_count >= MAX_SPRITES ) {
+		glBufferData( GL_ARRAY_BUFFER,
+				sprite_count * SPRITE_MESH_SIZE * sizeof (GLfloat ),
+				vpos_data, GL_DYNAMIC_DRAW );
+		glDrawArrays( GL_TRIANGLES, 0, sprite_count * 6 );
+		sprite_count = 0;
 	}
-	return quads;
 }
 
 int main() {
@@ -191,19 +236,35 @@ int main() {
 	glClearColor( 0.0, 0.0, 0.0, 1.0 );
 
 	while ( !glfwWindowShouldClose( window ) ) {
+		// static double previous_seconds = glfwGetTime();
+		// double current_seconds = glfwGetTime();
+		// double elapsed_seconds = current_seconds - previous_seconds;
+		// previous_seconds = current_seconds;
+
 		int w, h;
 		glfwGetFramebufferSize( window, &w, &h );
 		glViewport( 0, 0, w, h );
 
 		glClear( GL_COLOR_BUFFER_BIT );
 
-		// update_sprites();
-		int quads = draw_sprites();
+		for ( size_t i = 0; i < MAX_SPRITES; i++ ) {
+			if ( sprites[i].life > 0 ) {
 
-		glBufferData( GL_ARRAY_BUFFER,
-				MAX_SPRITES * SPRITE_MESH_SIZE * sizeof( GLfloat ),
-				sprite_vertices, GL_DYNAMIC_DRAW );
-		glDrawArrays( GL_TRIANGLES, 0, quads * 6 );
+				push_matrix();
+
+				trans( sprites[i].x, sprites[i].y );
+				rot( sprites[i].rotation );
+				sprites[i].rotation += 0.01;
+				drawRect( sprites[i].x, sprites[i].y, sprites[i].size, sprites[i].size );
+
+				pop_matrix();
+			}
+		}
+
+		glBufferSubData( GL_ARRAY_BUFFER, 0,
+				sprite_count * SPRITE_MESH_SIZE * sizeof (GLfloat ),
+				vpos_data);
+		glDrawArrays( GL_TRIANGLES, 0, sprite_count * 6 );
 
 		glfwPollEvents();
 		if ( GLFW_PRESS == glfwGetKey( window, GLFW_KEY_ESCAPE ) ) {
